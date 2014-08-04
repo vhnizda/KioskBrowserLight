ee5b941b0931adde997e82b123d47cf6b0fd4f17
Vaclav <vaclav.cz@gmail.com>
2014-08-04 13:23:18 -0400

    More testing to android layout. Moved all rotation to xml file, this cleans up the java code.

--- app/src/main/java/com/vaclavhnizda/kioskbrowserlight/Kiosk.java
+++ app/src/main/java/com/vaclavhnizda/kioskbrowserlight/Kiosk.java
@@ -9,6 +9,12 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.view.View;
 import android.app.ActionBar;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.FrameLayout;
+import android.view.Display;
+import android.graphics.Point;
+import android.widget.RelativeLayout;
+import android.view.ViewGroup;
 
 public class Kiosk extends Activity {
 
@@ -16,7 +22,7 @@ public class Kiosk extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Original code
-        //setContentView(R.layout.activity_kiosk);
+        setContentView(R.layout.activity_kiosk);
 
         //Hide all the status bars
         View decorView = getWindow().getDecorView();
@@ -28,24 +34,68 @@ public class Kiosk extends Activity {
         ActionBar actionBar = getActionBar();
         actionBar.hide();
 
-        //Webview & Rotation
-        WebView webView = new WebView(this);                    // Creates a new Webview
-        webView.getSettings().setJavaScriptEnabled(true);       // Enables Javascript for webpages
-        webView.setInitialScale(50);                            // Zooms out initially to 50%
-//        webView.getSettings().setTextZoom(3);                   // Zooms text size down
-        webView.setBackgroundColor(Color.BLACK);
-        webView.loadUrl("http://ts.transitscreen.com/index.php/screen/index/296500");
-        setContentView(webView);
 
-//        int x = webView.getMeasuredWidth();
-//        int y = webView.getMeasuredHeight();
-//        webView.setMinimumHeight(x);
-//        webView.setMinimumWidth(y);
-        //webView.setRotation(-45);
-        //webView.setFitsSystemWindows(true);
+//        // Test existing XML data
+//        WebView xmlWebView = (WebView)findViewById(R.id.testWebpageView);
+//        xmlWebView.loadUrl("www.google.com");
+//        xmlWebView.setRotation(20);
+
+
+//        //Attempt to rotate the screen - http://stackoverflow.com/questions/21355784/android-rotate-whole-layout
+        RelativeLayout mainLayout = (RelativeLayout) findViewById(R.id.main);
+        int w = mainLayout.getWidth();
+        int h = mainLayout.getHeight();
+
+//        mainLayout.setRotation(270.0f);
+        mainLayout.setTranslationX((w - h) / 2);
+        mainLayout.setTranslationY((h - w) / 2);
+//
+//        WebView xmlWebView = new WebView(this);
+//        xmlWebView.loadUrl("http://www.google.com");
+//        mainLayout.addView(xmlWebView);
+//
+//        ViewGroup.LayoutParams lp = (ViewGroup.LayoutParams) mainLayout.getLayoutParams();
+//        lp.height = w;
+//        lp.width = h;
+        mainLayout.requestLayout();
+
+
+
+
+//        // Gets the dimensions of the screen.
+//        Display display = getWindowManager().getDefaultDisplay();
+//        Point size = new Point();
+//        display.getSize(size);
+
+
+
+
+        // One idea of creating everything here..
+
+//        FrameLayout myFrame = new FrameLayout(this);
+//        myFrame.setBackgroundColor(Color.BLACK);
+//        int oldBottom = myFrame.getBottom();
+//        //myFrame.setRotation(90);
+//
+//
+//        //Webview & Rotation
+//        WebView webView = new WebView(this);                    // Creates a new Webview
+//        webView.getSettings().setJavaScriptEnabled(true);       // Enables Javascript for webpages
+//        webView.setInitialScale(60);                            // Zooms out initially to 50% for smaller screens
+//        webView.setRotation(90);
+////        webView.getSettings().setTextZoom(3);                   // Zooms text size down
+//        //webView.setBackgroundColor(Color.BLACK);
+//        webView.loadUrl("http://ts.transitscreen.com/index.php/screen/index/111");
+////        webView.loadUrl("http://ts.transitscreen.com/index.php/screen/index/296500");
+//        //webView.loadUrl("http://knowledgeboxes.bitbucket.org");
+//
+////        setContentView(webView);                                // How webpage get's pushed to device screen
+//
+//        //setContentView(webView);
+//        myFrame.addView(webView);
+//        setContentView(myFrame);
 
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
