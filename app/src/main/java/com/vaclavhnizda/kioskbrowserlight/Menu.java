aeec771c5254369f8b5a5a1607d78327d5121ae4
Vaclav <vaclav.cz@gmail.com>
2014-08-10 20:09:26 -0400

    Menu page option: URL

--- app/src/main/java/com/vaclavhnizda/kioskbrowserlight/Menu.java
+++ app/src/main/java/com/vaclavhnizda/kioskbrowserlight/Menu.java
@@ -1,11 +1,20 @@
 package com.vaclavhnizda.kioskbrowserlight;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by HnizdaV on 8/10/14.
  */
 public class Menu extends Activity{
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Original code - load layout
+        setContentView(R.layout.activity_menu);
+
+
+    }
 
 }
