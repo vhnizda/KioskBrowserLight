c478241752bd671de0e4d605b5d41c49798e2646
Vaclav <vaclav.cz@gmail.com>
2014-08-12 15:17:31 -0400

    Current setup: - URL changeable in menu - Sidebar hidden currently enabled. This prevents from the menu showing up.. need to work on hide/show animation. Then the menu can be used again.

--- app/src/main/java/com/vaclavhnizda/kioskbrowserlight/Kiosk.java
+++ app/src/main/java/com/vaclavhnizda/kioskbrowserlight/Kiosk.java
@@ -93,15 +93,15 @@ public class Kiosk extends Activity {
 
         }
 
-//        //Hide all the status bars
-//        View decorView = getWindow().getDecorView();
-//        // Hide the status bar.
-//        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-//        decorView.setSystemUiVisibility(uiOptions);
-//        // Remember that you should never show the action bar if the
-//        // status bar is hidden, so hide that too if necessary.
-//        ActionBar actionBar = getActionBar();
-//        actionBar.hide();
+        //Hide all the status bars
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
 
     }
 
--- app/src/main/java/com/vaclavhnizda/kioskbrowserlight/WebBrowser.java
+++ app/src/main/java/com/vaclavhnizda/kioskbrowserlight/WebBrowser.java
@@ -1,14 +1,7 @@
 package com.vaclavhnizda.kioskbrowserlight;
 
 import android.webkit.WebViewClient;
-import android.app.Activity;
-import android.content.SharedPreferences;
-import android.os.Bundle;
 import android.webkit.WebView;
-import android.widget.EditText;
-import android.content.Context;
-import android.view.View;
-import android.widget.TextView;
 
 /**
  * Created by HnizdaV on 8/12/14.
