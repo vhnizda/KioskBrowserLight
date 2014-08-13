0a86122567d81e348b22ac2129de9b45452a3a62
Vaclav <vaclav.cz@gmail.com>
2014-08-13 10:25:28 -0400

    Redesigned menu structure to include zoom value and rotation value. Next must link that to global values that can be adjusted.

--- app/src/main/java/com/vaclavhnizda/kioskbrowserlight/Kiosk.java
+++ app/src/main/java/com/vaclavhnizda/kioskbrowserlight/Kiosk.java
@@ -93,13 +93,12 @@ public class Kiosk extends Activity {
 
         }
 
-//        //Hide all the status bars
-//        View decorView = getWindow().getDecorView();
-//        // Hide the status bar.
-//        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-//        decorView.setSystemUiVisibility(uiOptions);
-//        // Remember that you should never show the action bar if the
-//        // status bar is hidden, so hide that too if necessary.
+        //--  Hide all the status bars  ---------------------------------------------------------//
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+//        // Hide the Action bar -> with menu button
 //        ActionBar actionBar = getActionBar();
 //        actionBar.hide();
 
